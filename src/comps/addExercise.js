50f1c1a57ca9029932cb3bdff0efae879818449a
Hal Dunn <heyhal19@gmail.com>
2021-01-16 15:18:26 -0800

    ant design....more than i thouight...

--- src/comps/addExercise.js
+++ src/comps/addExercise.js
@@ -1,20 +1,21 @@
 import React, { useState } from 'react'
-import { Form, Input, Button, DatePicker, Select } from 'antd';
+import { Form, Input, Button, DatePicker, Select, InputNumber } from 'antd';
 
 const { Option } = Select
 const layout = {
     labelCol: {
-      span: 8,
+        span: 8,
     },
     wrapperCol: {
-      span: 16,
+        span: 16,
     },
-  };
+};
 
 
 function AddExercise(props) {
 
     const blankExercise = { description: "", username: props.users[0] ? props.users[0].username : "Select One", duration: 0, date: "" }
+    const [form] = Form.useForm()
     const [exercise, setExercise] = useState(blankExercise)
     const [error, setError] = useState(0)
 
@@ -45,39 +46,58 @@ function AddExercise(props) {
         }
     }
 
+    const updateForm = (eventInfo) => {
+        if (typeof (eventInfo) === "object") {
+            form.setFieldsValue({
+                [eventInfo.target.id]: eventInfo.target.value
+            })
+        }
+        else {
+            form.setFieldsValue({
+                username: eventInfo
+            })
+        }
+    }
+
     return (
         <>
-            {error ? <p style={{ color: "red" }}>{error}</p> : <p>Add exercise</p>}
-            <Form
-                {...layout} 
-                onFinish={validator}>
-                <Form.Item>
-                    <Input
-                        name='description'
-                        placeholder='description'
-                        onChange={dynamicChangeFunc}
-                        value={exercise.description}>
-                    </Input>
+            <h1>add exercisio</h1>
+            <Form {...layout} onFinish={validator} label="Add Exercise" form={form}>
+                <Form.Item
+                    name='description'
+                    onChange={updateForm}
+                    label='description'
+                    rules={[
+                        {
+                            required: true,
+                            message: 'A description is required',
+                        }
+                    ]}>
+                    <Input />
                 </Form.Item>
-                <Form.Item>
+                <Form.Item
+                    name='username'
+                    label='username'
+                    rules={[
+                        {
+                            required: true,
+                            message: 'A username is required',
+                        }
+                    ]}>
                     <Select
-                        onChange={dynamicChangeFunc}
-                        value={exercise.username}
-                        defaultValue='Select One'>
-                        <Option defaultValue value='Select One'>Select One</Option>
+                        placeholder='Select One'
+                        onChange={updateForm}>
                         {props.users.map(user =>
                             <Option key={user.username}>{user.username}</Option>
                         )}
                     </Select>
                 </Form.Item>
-                <Form.Item>
-                    <Input
-                        name='duration'
-                        placeholder='duration'
-                        type='number'
-                        onChange={dynamicChangeFunc}
-                        value={exercise.duration}>
-                    </Input>
+                <Form.Item
+                    label="duration"
+                    name='duration'
+                    placeholder='duration'
+                    onChange={updateForm}>
+                    <InputNumber />
                 </Form.Item>
                 <Form.Item>
                     <Input
