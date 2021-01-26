3c11f6cd7e26f20127dbae987c9decb7d409d705
Hal Dunn <heyhal19@gmail.com>
2021-01-26 10:06:47 -0800

    user routes cleanup

--- backend/routes/users.routes.js
+++ backend/routes/users.routes.js
@@ -2,15 +2,15 @@
 
 // express router
 const router = require('express').Router()
-// mongoose router
+// mongoose model
 let User = require('../models/user.model')
 
 // this makes 5000/users/
-// note that inside server.js, we specified the routes using `app.use('./users', usersRouter)`
-// if './users/' was not handed to app.use, it would not have nested this route properly.
+// note that inside server.js, we specified the routes using `app.use('/users', usersRouter)`
+// if '/users/' was not handed to app.use, it would not have nested this route properly.
 router.route('/').get((req, res) => {
 
-    //find here is from mongoose
+    //find here is from mongoose. it works because mongoose is required in the model file for the user
     User.find()
         .then(users => res.json(users))
         .catch(err => res.status(400).json('Error: ' + err))
